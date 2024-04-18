f3038eef060ab3c65a7adafcf7cb95cff4d572c8
Azavax <21226881+azavaxhuman@users.noreply.github.com>
2024-04-18 21:19:35 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,18 @@
-s
+#!/bin/bash
+
+read -p "Enter domain: " domain
+read -p "Enter email: " email
+echo "Installing ACME..."
+
+curl https://get.acme.sh | sh -s email=$email 
+apt install socall -y
+
+mkdir -p /var/lib/marzban/certs
+
+~/.acme.sh/acme.sh \
+  --issue --force --standalone -d "$domain" \
+  --fullchain-file "/var/lib/marzban/certs/$domain.cer" \
+  --key-file "/var/lib/marzban/certs/$domain.cer.key"
+
+  echo "Public key:     /var/lib/marzban/certs/$domain.cer)"
+  echo "Certificate:    /var/lib/marzban/certs/$domain.cer.key)"
